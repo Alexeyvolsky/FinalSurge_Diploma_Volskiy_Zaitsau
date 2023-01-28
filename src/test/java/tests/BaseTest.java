7fa49f9c76fdb083cb43ce241a72dfc60f8cf065
Alexeyvolsky <aleksvolsky@gmail.com>
2023-01-28 14:55:03 +0300

    1

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,8 @@
+package tests;
+
+public class BaseTest {
+    protected final static String BASE_URL = "https://log.finalsurge.com/";
+    protected final static String USERNAME = "aleksvolsky@gmail.com";
+    protected final static String PASSWORD = "1234567890QwE";
+
+}
