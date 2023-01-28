e5c1310dd927b71e23790f559414b4daa1f5ad1a
Alexeyvolsky <aleksvolsky@gmail.com>
2023-01-28 15:45:23 +0300

    login

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,8 +1,40 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import pages.DashboardPage;
+import pages.LoginPage;
+
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
     protected final static String BASE_URL = "https://log.finalsurge.com/";
     protected final static String USERNAME = "aleksvolsky@gmail.com";
     protected final static String PASSWORD = "1234567890QwE";
+    protected WebDriver driver;
+    protected LoginPage loginPage;
+    protected DashboardPage dashboardPage;
+    @BeforeClass(alwaysRun = true)
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless");
+        options.addArguments("--ignore-certificate-errors");
+        options.addArguments("--disable-popup-blocking");
+        options.addArguments("--disable-notifications");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+        loginPage = new LoginPage(driver);
+        dashboardPage = new DashboardPage(driver);
+    }
+    @BeforeMethod(alwaysRun = true)
+    public void navigate() {
+        driver.get(BASE_URL);
+    }
 
 }
