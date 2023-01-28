0af76e7e0e8efbcf62900017df71ebc1e2aa86a2
Alexeyvolsky <aleksvolsky@gmail.com>
2023-01-28 23:09:20 +0300

    3

---
+++ src/test/java/elements/Input.java
@@ -0,0 +1,20 @@
+package elements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Input extends BaseElement{
+
+    private final static String INPUT_LOCATOR = "%s";
+
+
+    public Input(WebDriver driver) {
+        super(driver);
+    }
+    public void setValue(String id, String value){
+        WebElement input = driver.findElement(By.id(String.format(INPUT_LOCATOR, id)));
+        scrollIntoView(input);
+        input.sendKeys(value);
+    }
+}
